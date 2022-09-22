b9bacd075279305d86d4ab238ab885aeac9c99cd
tkwkdzz <113489388+tkwkdzz@users.noreply.github.com>
2022-09-22 16:16:22 +0900

    MakeCode 프로젝트용 기초 파일

---
+++ main.ts
@@ -0,0 +1,24 @@
+input.onButtonPressed(Button.A, function () {
+    while (초기화 == 0) {
+        basic.showString("password")
+        if (밝기 <= 30) {
+            basic.showIcon(IconNames.Happy)
+        }
+        basic.showString("HI")
+        초기화 = 1
+    }
+    music.playMelody("E D C D E E E D ", 140)
+    music.stopAllSounds()
+    music.playMelody("D D E G G E D C ", 120)
+    music.stopAllSounds()
+    music.playMelody("D E E E D D E D ", 120)
+    music.stopAllSounds()
+    music.playTone(262, music.beat(BeatFraction.Whole))
+})
+let 밝기 = 0
+let 초기화 = 0
+basic.showString("HI")
+basic.forever(function () {
+    밝기 = input.lightLevel()
+    초기화 = 0
+})
